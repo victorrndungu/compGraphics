034ccb712d4d5a25a8750630b2d5d91be4f12063
maximillian mwenda <143392319+maximillian15@users.noreply.github.com>
2024-10-18 11:24:36 +0300

    Add files via upload

---
+++ Roof.py
@@ -0,0 +1,16 @@
+# 2.Roof (triangle for left side)
+ctx.move_to(180, 320)
+ctx.line_to(300, 200)
+ctx.line_to(420, 320)
+
+ctx.move_to(165, 305)
+ctx.line_to(300, 175)
+ctx.line_to(435, 305)
+
+ctx.move_to(180, 320)
+ctx.line_to(165, 305)
+
+ctx.move_to(420, 320)
+ctx.line_to(435, 305)
+ctx.stroke()
+
